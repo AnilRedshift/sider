01c580e419ca8b68639e3ec006ba3afe15806511
Anil Kulkarni <anil.kulkarni@outlook.com>
2019-03-10 16:24:06 -0700

    Update mix

--- mix.exs
+++ mix.exs
@@ -1,24 +1,38 @@
 defmodule Sider.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "In-memory key/value store with expiring keys"
+  @github "https://github.com/AnilRedshift/sider"
+
   def project do
     [
       app: :sider,
-      version: "0.1.0",
+      version: @version,
+      description: @description,
+      docs: [
+        source_ref: "v#{@version}",
+        main: "Sider"
+      ],
+      source_url: @github,
+      package: [
+        name: :sider,
+        maintainers: ["Anil Kulkarni"],
+        licenses: ["MIT"],
+        links: %{"Github" => @github}
+      ],
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
